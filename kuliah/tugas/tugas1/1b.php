2a2e0458a338c2beb79e2656a05913491e6764c1
Mohzu <mochzuhdi04@gmail.com>
2023-02-16 17:33:05 +0700

    mengerjakan tugas 1

---
+++ kuliah/tugas/tugas1/1b.php
@@ -0,0 +1,23 @@
+<?php 
+$nrp = "1";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        Aku adalah angka <b><?php echo "$nrp" ?></b><br>
+        Jika aku dikali 5, maka aku sekarang menjadi <b><?php echo $nrp * 5 ?></b><br>
+        Jika aku dibagi 2, maka aku sekarang menjadi <b><?php echo 5 / 2 ?></b><br>
+        Jika aku ditambah 75, maka aku sekarang menjadi <b><?php echo 2.5 + 75 ?></b><br>
+        Jika aku dikurang 20, maka aku sekarang menjadi <b><?php echo 77.5 - 20 ?></b><br>
+
+    </p>
+</body>
+</html>
